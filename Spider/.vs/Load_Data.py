fdf11cf0cce2ab6c6b711d0cca053524a1c600f8
Anna <Markelova.am@phystech.edu>
2021-05-22 00:50:29 +0300

    Taking_Dataset

--- Spider/.vs/Load_Data.py
+++ Spider/.vs/Load_Data.py
@@ -1,24 +1,11 @@
-def Taking_Dataset() :
-    import numpy as np
-    import tensorflow as tf
+import numpy
+import tensorflow
+
+def Taking_Dataset():
 
     DATA_URL = 'https://storage.googleapis.com/tensorflow/tf-keras-datasets/mnist.npz'
 
-    path = tf.keras.utils.get_file('mnist.npz', DATA_URL)
-    with np.load(path) as data:
-        train_examples = data['x_train']
-        train_labels = data['y_train']
-        test_examples = data['x_test']
-        test_labels = data['y_test']
+    path = tensorflow.keras.utils.get_file('mnist.npz', DATA_URL)
 
-    train_dataset = tf.data.Dataset.from_tensor_slices((train_examples, train_labels))
-    test_dataset = tf.data.Dataset.from_tensor_slices((test_examples, test_labels))
-
-    BATCH_SIZE = 64
-    SHUFFLE_BUFFER_SIZE = 100
-
-    train_dataset = train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
-    test_dataset = test_dataset.batch(BATCH_SIZE)
-
-    return
+    return numpy.load(path)
 
