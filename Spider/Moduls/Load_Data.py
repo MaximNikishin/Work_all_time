129def06835b0d868d1d13edb3de45225293ed96
Vasimilian <84583465+Vasimilian@users.noreply.github.com>
2021-05-22 17:20:40 +0300

    Update Load_Data.py

--- Spider/Moduls/Load_Data.py
+++ Spider/Moduls/Load_Data.py
@@ -1,10 +1,14 @@
 
 
-def Taking_Dataset():
+def Taking_Dataset(tfashion = false, tconv = false, other = false):
+    if(tfashion == true) : 
+        return tf.keras.datasets.fashion_mnist.load_data()
+    if(tconv == true) :
+        return datasets.cifar10.load_data()
+    
+    #DATA_URL = 'https://storage.googleapis.com/tensorflow/tf-keras-datasets/mnist.npz'
 
-    DATA_URL = 'https://storage.googleapis.com/tensorflow/tf-keras-datasets/mnist.npz'
+    #path = tf.keras.utils.get_file('mnist.npz', DATA_URL)
 
-    path = tf.keras.utils.get_file('mnist.npz', DATA_URL)
-
-    return np.load(path)
+    #return np.load(path)
 
